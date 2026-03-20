ecb042b8b24b7f51e106f1741770d639b604d45f
Jade Darton <jadeedarton@gmail.com>
2026-03-20 08:15:08 +0000

    Fix missing views import in project urls

--- ukwot_app/urls.py
+++ ukwot_app/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 from django.contrib.auth import views as auth_views
+from ukwot import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
